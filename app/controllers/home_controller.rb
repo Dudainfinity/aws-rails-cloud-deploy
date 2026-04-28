464dea4e45643d246c716a824971ab4846f76377
Dudainfinity <dudainfinitysch@gmail.com>
2026-04-28 13:00:11 -0300

    feat: nova UI, status do servidor em tempo real e CI/CD com lint, testes e health check

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,25 @@
 class HomeController < ApplicationController
   def index
+    @ruby_version = RUBY_VERSION
+    @rails_version = Rails.version
+    @environment = Rails.env
+    @uptime = uptime
+    @db_connected = db_connected?
+  end
+
+  private
+
+  def uptime
+    seconds = Process.clock_gettime(Process::CLOCK_MONOTONIC).to_i
+    hours   = seconds / 3600
+    minutes = (seconds % 3600) / 60
+    "#{hours}h #{minutes}m"
+  end
+
+  def db_connected?
+    ActiveRecord::Base.connection.execute("SELECT 1")
+    true
+  rescue StandardError
+    false
   end
 end
